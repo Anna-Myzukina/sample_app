49bb4549391de344185af2618ff2b2dc05077b23
Anna Myzukina <anna.muzykina83@gmail.com>
2019-10-14 20:37:42 +0300

    Modified routes.rb, test_helper.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get  'static_pages/about'
-  root 'application#hello'
-  get  'static_pages/contact'
-end
+    root 'static_pages#home'
+    get  'static_pages/home'
+    get  'static_pages/help'
+    get  'static_pages/about'
+  end
