37721bcd86f3e76b28138474dfd66771ea07ce6e
Anna Myzukina <anna.muzykina83@gmail.com>
2019-11-04 23:10:24 +0200

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  
+  get 'users/new'
+
     root 'static_pages#home'
     get  'static_pages/home'
     get  'static_pages/help'
     get  'static_pages/about'
+    get  'static_pages/contact'
   end
