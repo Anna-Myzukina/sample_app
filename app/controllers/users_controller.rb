2964fb95c998526a5d232cd0176f14d284f06c28
Anna Myzukina <anna.muzykina83@gmail.com>
2019-11-06 11:42:02 +0200

    Add styles to create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
-=======
 
->>>>>>> creating-users
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def new
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,7 @@
 require 'test_helper'
-
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get signup_path
     assert_response :success
   end
-
 end
