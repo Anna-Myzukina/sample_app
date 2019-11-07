b55fe2ac438923a1196529395c7211c83b06094a
Anna Myzukina <anna.muzykina83@gmail.com>
2019-11-07 19:32:50 +0200

    Add login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       # Log the user in and redirect to the user's show page.
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_to user
     else
       flash[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new'
